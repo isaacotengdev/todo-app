a6f671efa83a78e68484bdffa4b5d091c9e32f05
isaacotengdev <isaacnewtonoteng@gmail.com>
2024-11-07 22:59:48 +0000

    first-commit

---
+++ src/ToDoApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function ToDoApp() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTask = () => {
+    if (input.trim()) {
+      setTasks([...tasks, input]);
+      setInput('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task} <button onClick={() => removeTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoApp;
